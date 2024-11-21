29f4e917ca7b88e3ec316d76368b9a6bfe970bb5
muruganjs <ksmurugan.murugan@gmail.com>
2024-11-21 12:33:14 +0530

    mohammed

--- src/org/gitproject/GitHub.java
+++ src/org/gitproject/GitHub.java
@@ -5,7 +5,7 @@ public class GitHub {
 	public static void main(String[] args) {
 		System.out.println("Rajesh completed the work");
 		System.out.println("Karthik completed the work");
-		System.out.println("Karthik completed the work");
+		System.out.println("mohammed completed the work");
 
 		
 	}
